d07f942c6e745c8953fb7dbc3ba5f1989535c65c
Stuart Olsen <so1132@txstate.edu>
2014-09-20 01:20:16 -0500

    Revert "A different approach to formatting ints"
    This reverts commit 64da0fbde529931fd90458d0bf0ffcc88d43140f.

--- format_int/format_int.cc
+++ format_int/format_int.cc
@@ -6,38 +6,38 @@ char clookup (std::uint_fast8_t digit)
 	return (digit < 10) ? ('0' + digit) : ('A' + (digit - 10));
 }
 
-char* format_uint_2special (char* buffer_end, std::uintmax_t value, std::uint_fast8_t base_power)
+std::size_t format_uint_2special (char* buffer, std::uintmax_t value, std::uint_fast8_t base_power)
 {
 	std::uintmax_t mask = 0;
 	--mask;
 	mask >>= (sizeof (std::uintmax_t) * CHAR_BIT) - base_power;
 
-	char* cursor = buffer_end - 1;
-	*cursor = '\0';
-
+	char* cursor = buffer;
 	do
 	{
-		--cursor;
 		*cursor = clookup (value & mask);
 		value >>= base_power;
+		++cursor;
 	}
 	while (value != 0);
 
-	return cursor;
+	std::reverse (buffer, cursor);
+	*cursor = '\0';
+	return cursor - buffer;
 }
 
-char* format_uint_generic (char* buffer_end, std::uintmax_t value, std::uint_fast8_t base)
+std::size_t format_uint_generic (char* buffer, std::uintmax_t value, std::uint_fast8_t base)
 {
-	char* cursor = buffer_end - 1;
-	*cursor = '\0';
-
+	char* cursor = buffer;
 	do
 	{
-		--cursor;
 		*cursor = clookup (value % base);
 		value /= base;
+		++cursor;
 	}
 	while (value != 0);
 
-	return cursor;
+	std::reverse (buffer, cursor);
+	*cursor = '\0';
+	return cursor - buffer;
 }
